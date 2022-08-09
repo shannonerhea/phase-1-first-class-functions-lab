0706ee7ead69ffd04acf302bee1b801a7490887e
shannone <shannone426@gmail.com>
2022-08-09 06:16:01 -0400

    advanced functions

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+// const returnFirstTwoDrivers(arr) {
+//     const firstTwo = (arr) => {arr.slice(0, 2)};
+// };
+
+//OG arr (['Antonia', 'Nuru', 'Amari', 'Mo']);
+const returnFirstTwoDrivers = (arr) => {
+    return arr.slice(0, 2);
+    
+  }
+ //returnLastTwoDrivers()
+ //1) should return an array of the last two drivers
+const returnLastTwoDrivers = (arr) => {};
