92b7dbc5cd50da1678d3bd9c7044e9324be5f84a
shannone <shannone426@gmail.com>
2022-08-09 08:07:18 -0400

    advanced functions

--- index.js
+++ index.js
@@ -17,3 +17,37 @@ const returnLastTwoDrivers = (arr) => {
 // 1) has the `returnFirstTwoDrivers` function to as its first element
 //const selectingDrivers = (drivers) => {};
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// const createFareMultiplier = (num) => {
+//    // return fareMultiplier(multiplier) {
+//       //  return (num) * (multiplier);
+//       const fairDoubler = createFareMultiplier()
+//       return fairDoubler;
+    
+
+// };
+// 
+//const createFareMultiplier(multiplier)
+// function createFareMultiplier(multiplier)) {
+//     return function(fare) {
+//         return fare * multiplier;
+//     };
+// };
+// function createFareMultiplier(multiplier) {
+//     return function(fare) {
+//         return fare * multiplier;
+//     }
+// }
+//**OR**/
+const createFareMultiplier = multiplier => function(fare) { return fare * multiplier };
+
+// funct doubles fare
+//  const fareDoubler = () => {}; 
+//  const fairDoubler = createFareMultiplier(fare) {
+//     return " wtf"
+//  };
+const fareDoubler(singleArg) {
+    const newFunct = function(fare) {
+        return //a val created by passing the arg collected in fareDoubler into the new variable declared in const
+    };
+};
